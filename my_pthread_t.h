29327f8110378d9d49a5219648546d232b926f61
Andrew Khazanovich <akhazanovich+GIT@gmail.com>
2017-02-26 13:51:54 -0500

    changes

--- my_pthread_t.h
+++ my_pthread_t.h
@@ -37,8 +37,8 @@ enum mutex_status {LOCKED, UNLOCKED}
 typedef struct my_pthread_t {
     ucontext_t * thread_context;
     struct my_pthread_t * next;
-    struct my_pthread_mutex_t * mutex_flag; //NULL if no mutex
-    struct my_pthread_t * waitlist ;        // List of 
+    struct my_pthread_mutex_t * mutex_flag; // NULL if no mutex
+    struct my_pthread_t * waitlist ;        // List of threads that this thread is waiting on 
     struct my_pthread_t * joinlist;         // List of threads that are joined to execution of this thread
     enum thread_status status;
     int priority_level;
